f1385ed50ee706286a7b45c6be172e16724f79c3
derrickhua <derrickhua6@gmail.com>
2024-03-25 12:54:35 -0400

    Minor fix

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/quote', (req, res) => {
 });
 
 // Start the server
-const port = 3000;
+const port = 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
