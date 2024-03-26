6989b60fd020b324043916535dfd9650b07a50f6
Derrick Paul Zamora Hua <108483521+derrickhua@users.noreply.github.com>
2024-03-26 14:55:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/quote', (req, res) => {
 });
 
 // Start the server
-const port = 8080;
+const port = 80;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
